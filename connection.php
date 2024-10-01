5272c90ce6ecbfddae0ad6e96f22e88d4a3c69fe
Lantern Elf <lantern4k@gmail.com>
2024-10-01 11:57:44 +0700

    connection

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = "basisperpustakaan";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";

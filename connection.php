2abc53178ed41b819ca7e6b84338065a5de50018
Lantern Elf <lantern4k@gmail.com>
2024-10-02 12:34:56 +0700

    Adding Style

--- connection.php
+++ connection.php
@@ -4,11 +4,11 @@ $username = "root";
 $password = "";
 $dbName = "basisperpustakaan";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbName);
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbName);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
 include 'connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Home | Perpustakaan</title>
+</head>
+
+<body>
+
+</body>
+
+</html>

34a84c1e4797a014ba10c1ca4a4650d7397d9947
Lantern Elf <114974407+lantern-elf@users.noreply.github.com>
2024-10-03 08:16:03 +0700

    Updating "Tambah"

--- index.php
+++ index.php
@@ -9,15 +9,12 @@ include 'connection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="components/navbar.css">
-    <link rel="stylesheet" href="components/header.css">
     <title>Home | Perpustakaan</title>
 </head>
 
 <body>
     <?php
-    include 'components/navbar.html';
-    include 'components/header.html'
+    include 'components/navbar.html'; //Navbar
     ?>
 </body>
 
--- tambah.php
+++ tambah.php
@@ -20,20 +20,33 @@ include 'connection.php';
     <form action="tambah_prosess.php" method="post">
         <label for="">Judul: </label><br>
         <input type="text" name="judul"><br>
+        <br>
+
         <label for="">Penulis: </label><br>
         <input type="text" name="penulis"><br>
+        <br>
+
         <label for="">Penerbit: </label><br>
         <input type="text" name=""><br>
+        <br>
+
         <label for="">Tahun Terbit: </label><br>
         <input type="text" name=""><br>
+        <br>
+
         <label for="">Jumlah Halaman</label><br>
         <input type="text" name=""><br>
+        <br>
+
         <label for="">Lokasi</label><br>
         <input type="text" name=""><br>
-
         <br>
+
         <label for="">Gambar: </label><br />
-        <input type="image" alt="submit" name="gambar"><br>
+        <input type="file" src="assets/1337310-200.png" alt="" name="gambar" width="64px" height="64px"><br>
+        <br>
+
+        <input type="submit" value="Submit">
     </form>
 </body>
 
